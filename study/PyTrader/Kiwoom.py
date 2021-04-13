5f8a28cc251b4b10ede4db30135d14218adbfae9
kyusangchoi <xjordan@gmail.com>
2021-04-13 14:27:26 +0900

    18강 시작

---
+++ study/PyTrader/Kiwoom.py
@@ -0,0 +1,28 @@
+import sys
+import time
+import pandas as pd
+import sqlite3
+
+from PyQt5.QtWidgets import *
+from PyQt5.QAxContainer import *
+from PyQt5.QtCore import *
+
+TR_REQ_TIME_INTERVAL = 0.2
+
+class Kiwoom(QAxWidget):
+    def __init__(self):
+        super().__init__()
+        self._create_kiwoom_instance()
+        self._set_signal_slots()
+
+    def _create_kiwoom_instance(self):
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+    def _set_signal_slots(self):
+        self.OnEventConnect.connect(self._event_connect)
+        self.OnReceiveTrData.connect(self._receive_tr_data)
+
+    def comm_connect(self):
+        self.dynamicCall("CommConnect()")
+        self.login_event_loop = QEventLoop()
+        self.login_event_loop.exec_()
---
+++ study/kiwoom-version-master/run.bat
@@ -0,0 +1,4 @@
+@echo off
+cd %~dp0
+C:\ProgramData\Anaconda3/python version.py
+C:\ProgramData\Anaconda3/python auto_on.py
